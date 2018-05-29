c41481c090b334681ad82bb4d011bd4095c18d30
Luis Cortes <lacortes@cpp.edu>
2018-05-29 13:36:42 -0700

    Working Version, without letter effects.

--- js/src/main.js
+++ js/src/main.js
@@ -4,73 +4,97 @@ $(document).ready(function () {
         let secondLen;
     
         if (names.length > 0) {
+
+            let randomButton = $("#random-button");
+            if (randomButton.text() == "Go Again") {
+                randomButton.text("Choose");
+
+                cssSelector = anime({
+                    targets: '.spin-me',
+                    scale: 1,
+                    rotate: '2turn'
+                });
+            }
+
             let name = getRandomName().split(" ");
 
             console.log(name);
 
             // Change html
             $(".word").first().text(name[0]);
-            $(".word").last().text(name[1]);
+            $(".word").last().text("");
 
             firstLen = name[0].length;
-            secondLen = name[1].length;
+            // secondLen = name[1].length;
 
         } else {
+            console.log("DONE");
+
+            $('#random-button').text("Go Again");
+            $('.first-name').text("Again?");
+
+            cssSelector = anime({
+                targets: '.spin-me',
+                scale: 2,
+                rotate: '2turn'
+            });
+
             names = namesCopy.slice();
         }
 
         // Wrap each letter in its own span
-        $('.name-display').each(function () {
-            $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-        });
+        // $('.name-display').each(function () {
+        //     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+        // });
 
         // Animate
-        let animate = anime.timeline({
-                        loop: true
-                    }).add({
-                        targets: '.name-display .letter',
-                        scale: [4, 1],
-                        opacity: [0, 1],
-                        translateZ: 0,
-                        easing: "easeOutExpo",
-                        duration: 950,
-                        delay: function (el, i) {
-                            return 70 * i;
-                        }
-                    }).add({
-                        targets: '.name-display',
-                        opacity: 0,
-                        duration: 1000,
-                        easing: "easeOutExpo",
-                        delay: 1000
-                    });
+        // let animate = anime.timeline({
+        //                 loop: false,
+        //                 autoplay: true
+        //             }).add({
+        //                 targets: '.name-display .letter',
+        //                 scale: [4, 1],
+        //                 opacity: [0, 1],
+        //                 translateZ: 0,
+        //                 easing: "easeOutExpo",
+        //                 duration: 950,
+        //                 delay: function (el, i) {
+        //                     return 70 * i;
+        //                 }
+        //             }).add({
+        //                 targets: '.name-display',
+        //                 opacity: 0,
+        //                 duration: 1000,
+        //                 easing: "easeOutExpo",
+        //                 delay: 1000
+        //             });
         
 
-        window.setTimeout(function () {
-            let firstName = "";
-            let lastName = "";
+        // window.setTimeout(function () {
+        //     let firstName = "";
+        //     let lastName = "";
 
-            // Get each letter
-            $('.name-display .letter').each(function (index) {
-                console.log($(this).text());
+        //     // Get each letter
+        //     $('.name-display .letter').each(function (index) {
+        //         console.log($(this).text());
                 
-                let count = index % (firstLen + secondLen) + 1;
+        //         let count = index % (firstLen + secondLen) + 1;
 
-                if (count <= firstLen) {
-                    firstName  = firstName + $(this).text();
-                } else {
-                    lastName = lastName + $(this).text();
-                }
+        //         if (count <= firstLen) {
+        //             firstName  = firstName + $(this).text();
+        //         } else {
+        //             lastName = lastName + $(this).text();
+        //         }
 
-            });
+        //     });
 
-            // Remove span elements (letters)
-            $('.name-display').empty();
+        //     // Remove span elements (letters)
+        //     $('.name-display').empty();
 
-            // Bring back only 2 spans
-            $('.name-display').html('<span class="word first-name">'+firstName+" "+'</span>' 
-                                    + '<span class="word last-name">'+lastName+'</span>');
-        }, 3000);
+        //     // Bring back only 2 spans
+        //     $('.name-display').html('<span class="word first-name">'+firstName+" "+'</span>' 
+        //                             + '<span class="word last-name"> </span>');
+        // }, 2000);
     
     }
 
@@ -81,10 +105,27 @@ $(document).ready(function () {
     }
 
     let names = [
-        "First1 Last1",
-        "First2 Last2",
-        "First3 Last3",
-        "First4 Last4"
+        "Brandon",
+        "Guadalupe",
+        "Alexandra",
+        "Christopher", 
+        "Charles",
+        "Dezire",
+        "Malei'a",
+        "Hayden",
+        "Draven",
+        "Andres",
+        "Phillip",
+        "London",
+        "Michael",
+        "Ayla",
+        "Olivia",
+        "Justin",
+        "Aiden",
+        "Eliana",
+        "Eddie",
+        "Zaylee",
+        "Emily"
     ];
 
     let namesCopy = names.slice();
